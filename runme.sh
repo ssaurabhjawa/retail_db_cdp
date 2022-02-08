aa68dc7a9eef31db8e9eb64af4e1907e3f25230e
saurabhjawa <jai_dev@macs-Mac-mini.local>
2022-02-08 10:11:15 +0530

    all

--- runme.sh
+++ runme.sh
@@ -1,7 +1,6 @@
 export PYSPARK_PYTHON=python3
 export ENVIRON=PROD
 export user=${USER}
-export SRC_DIR="/home/$user/retail-topic/"
+export SRC_DIR="/home/$user/retail_db_cdp/src/main/resources/retail-topic/"
 export APP_NAME='retail_db_poc'
-unzip -d src/main/resources/retail-topic.zip
 src/main/python/kafka_app.py
